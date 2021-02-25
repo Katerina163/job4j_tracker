897fb1b2474928c632613fe41cc1f22896f88c56
Nefedova <Katerina163>
2021-02-25 13:41:31 +0400

    1.5. Поля объекта [475130]

--- src/main/java/ru/job4j/tracker/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Cat.java
@@ -1,8 +1,32 @@
 package ru.job4j.tracker.ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.food);
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        System.out.println("The name of the cat is " + gav.name);
+        System.out.print("Cat food is a ");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        System.out.println("The name of the cat is " + black.name);
+        System.out.print("Cat food is a ");
+        black.show();
     }
 }
