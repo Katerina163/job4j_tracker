ce70fd337493c2a047ff2d0ce7495d6808a78678
Nefedova <Katerina163>
2021-03-03 15:01:10 +0400

    0.2. Кидаем исключение - throw new RuntimeException [475130]

--- src/main/java/ru/job4j/tracker/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/tracker/ru/job4j/ex/Fact.java
@@ -6,13 +6,13 @@ public class Fact {
 
     public static int calc(int n) {
         int rsl = 1;
-        if (n < 0) {
-            throw new IllegalArgumentException("The number must not be negative");
-        } else {
+        if (n >= 0) {
             for (int index = 1; index <= n; index++) {
                 rsl *= index;
             }
-        }
+        } else {
+                throw new IllegalArgumentException("The number must not be negative");
+            }
         return rsl;
     }
 }
