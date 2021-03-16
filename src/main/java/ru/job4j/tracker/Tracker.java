0f294217c5e28a3d562fa518ef2910f466128627
Nefedova <Katerina163>
2021-03-16 11:33:06 +0400

    6. Изменить программу Tracker из 2-го модуля [475130]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,33 +1,34 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    private final ArrayList<Item> items = new ArrayList<Item>(100);
     private int ids = 1;
     private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
     }
 
     public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+        return items.toArray(new Item[items.size()]);
     }
 
     public Item[] findByName(String key) {
-        Item[] array = new Item[items.length];
+        Item[] array = new Item[items.size()];
         int i = 0;
         for (int index = 0; index < size; index++) {
-            if (items[index].getName().equals(key)) {
-                array[i] = items[index];
+            if (items.get(index).getName().equals(key)) {
+                array[i] = items.get(index);
                 i++;
             }
 
@@ -41,7 +42,7 @@ public class Tracker {
         item.setId(id);
         boolean rsl = index != -1;
         if (rsl) {
-            items[index] = item;
+            items.set(index, item);
         }
         return rsl;
     }
@@ -49,7 +50,7 @@ public class Tracker {
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
+            if (items.get(index).getId() == id) {
                 rsl = index;
                 break;
             }
@@ -62,7 +63,7 @@ public class Tracker {
         boolean rsl = ind != -1;
         if (rsl) {
             System.arraycopy(items, ind + 1, items, ind, size - ind - 1);
-            items[size - 1] = null;
+            items.set(size - 1, null);
             size--;
         }
         return rsl;
