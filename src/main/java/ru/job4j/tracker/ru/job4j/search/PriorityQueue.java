3cdde7ba80e14aebe4ba49ae5cbb14d620e9c38a
Nefedova <Katerina163>
2021-03-16 09:20:19 +0400

    2. Очередь с приоритетом на LinkedList [475130]

--- src/main/java/ru/job4j/tracker/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/tracker/ru/job4j/search/PriorityQueue.java
@@ -12,13 +12,11 @@ public class PriorityQueue {
      */
     public void put(Task task) {
         int index = 0;
-        for (Task element : tasks) {
-            if (element.getPriority() < task.getPriority()) {
-                index++;
-            } else {
-               tasks.add(index, element);
-               break;
+        for (var element : tasks) {
+            if (task.getPriority() < element.getPriority()) {
+                break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
