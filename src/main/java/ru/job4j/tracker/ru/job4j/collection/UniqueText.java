e0995fe466f36f7553e4a6caa8ea4d46b5d70e5e
Nefedova <Katerina163>
2021-03-17 10:22:19 +0400

    2. Идентичные тексты [475130]

---
+++ src/main/java/ru/job4j/tracker/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker.ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String i: origin) {
+            check.add(i);
+        }
+        for (String i: text) {
+            return rsl = check.contains(i);
+        }
+        return rsl;
+    }
+}
