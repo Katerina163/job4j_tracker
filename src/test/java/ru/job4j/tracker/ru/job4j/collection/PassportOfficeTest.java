51d3fc4ad28a4bae8e90ff9625fa7ecc55c8e79f
Nefedova <Katerina163>
2021-03-17 15:24:57 +0400

    1. Паспорт и жители [475130]

--- src/test/java/ru/job4j/tracker/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/tracker/ru/job4j/collection/PassportOfficeTest.java
@@ -12,4 +12,15 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen);
+        office.add(citizen2);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
 }
